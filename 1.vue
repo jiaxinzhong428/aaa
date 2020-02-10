82d6a854a45ea7ea14a67afdf957c1705c562231
e <jiaxinzhong428@163.com>
2020-02-10 14:38:44 +0800

    vue

---
+++ 1.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        };
+    },
+    computed: {
+
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    watch: {
+
+    },
+    methods: {
+
+    },
+    components: {
+
+    },
+};
+</script>
+
+<style scoped lang="less">
+
+</style>
